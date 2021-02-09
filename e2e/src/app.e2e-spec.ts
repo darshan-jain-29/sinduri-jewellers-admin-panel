55263a3227b4275e97d768ce2fc4fdfbebb5e7c2
darshan-jain-29 <darshan.jain.29@gmail.com>
2021-02-09 17:25:09 +0530

    adding new files of admin

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('backend-app app is running!');
+    expect(page.getTitleText()).toEqual('sindhuri-jewellers-admin-panel app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/backend-app'),
+      dir: require('path').join(__dirname, './coverage/sindhuri-jewellers-admin-panel'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
